d080c247e290671b49ed6e34bbe56a11022ec0f9
Ex0oleg <olegexo@gmail.com>
2023-03-05 22:10:35 +0200

    Initial commit

---
+++ InfoSecTask2/InfoSecTask2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace InfoSecTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ///
+            Console.WriteLine();
+        }
+    }
+}
