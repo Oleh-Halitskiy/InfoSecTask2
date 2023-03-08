945d2c2398b084020e311a263a49d8c84cf503cc
Ex0oleg <olegexo@gmail.com>
2023-03-08 21:15:46 +0200

    added CFB and CBC encryption

--- InfoSecTask2/InfoSecTask2/DecryptionFunctions.cs
+++ InfoSecTask2/InfoSecTask2/DecryptionFunctions.cs
@@ -8,12 +8,122 @@ namespace InfoSecTask2
 
         public static void CBC()
         {
-            throw new NotImplementedException();
+            Console.Write("Enter 1 for encryption, 2 for decryption: ");
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.Write("Enter plaintext: ");
+                string plainText = Console.ReadLine();
+                Console.Write("Enter key: ");
+                string key = Console.ReadLine();
+                Console.Write("Enter IV: ");
+                string IV = Console.ReadLine();
+
+                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+                byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                byte[] IVbytes = Encoding.UTF8.GetBytes(IV);
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Mode = CipherMode.CBC;
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.KeySize = 128;
+                    aes.Key = keyBytes;
+                    aes.IV = IVbytes;
+
+                    ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                    byte[] encryptedBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+
+                    Console.WriteLine(Convert.ToBase64String(encryptedBytes));
+                }
+            }
+            else if (input == "2")
+            {
+                Console.Write("Enter encrypted text: ");
+                string encryptedString = Console.ReadLine();
+                Console.Write("Enter key: ");
+                string key = Console.ReadLine();
+                Console.Write("Enter IV: ");
+                string IV = Console.ReadLine();
+
+                byte[] encryptedBytes = Convert.FromBase64String(encryptedString);
+                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+                byte[] IVbytes = Encoding.UTF8.GetBytes(IV);
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Mode = CipherMode.CBC;
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.KeySize = 128;
+                    aes.Key = keyBytes;
+                    aes.IV = IVbytes;
+
+                    ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                    byte[] decryptedBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+
+                    Console.WriteLine(Encoding.UTF8.GetString(decryptedBytes));
+                }
+            }
         }
 
         public static void CFB()
         {
-            throw new NotImplementedException();
+            Console.Write("Enter 1 for encryption, 2 for decryption: ");
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.Write("Enter plaintext: ");
+                string plainText = Console.ReadLine();
+                Console.Write("Enter key: ");
+                string key = Console.ReadLine();
+                Console.Write("Enter IV: ");
+                string IV = Console.ReadLine();
+
+
+                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+                byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                byte[] IVbytes = Encoding.UTF8.GetBytes(IV);
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Mode = CipherMode.CFB;
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.KeySize = 128;
+                    aes.Key = keyBytes;
+                    aes.IV = IVbytes;
+                    ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                    byte[] encryptedBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+
+                    Console.WriteLine(Convert.ToBase64String(encryptedBytes));
+                }
+            }
+            else if (input == "2")
+            {
+                Console.Write("Enter encrypted text: ");
+                string encryptedString = Console.ReadLine();
+                Console.Write("Enter key: ");
+                string key = Console.ReadLine();
+                Console.Write("Enter IV: ");
+                string IV = Console.ReadLine();
+
+                byte[] encryptedBytes = Convert.FromBase64String(encryptedString);
+                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+                byte[] IVbytes = Encoding.UTF8.GetBytes(IV);
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Mode = CipherMode.CFB;
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.KeySize = 128;
+                    aes.Key = keyBytes;
+                    aes.IV = IVbytes;
+
+                    ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                    byte[] decryptedBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+
+                    Console.WriteLine(Encoding.UTF8.GetString(decryptedBytes));
+                }
+            }
         }
 
         public static void ECB()
